3210d3909bfed26f110fbec3a68ea7598d4fdd05
YatharthKaushal <yatharthkaushal@proton.me>
2025-09-30 13:24:40 +0530

    render ready

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,10 +10,12 @@ app.get("/", (req, res) => {
 app.use(json());
 
 app.post("/data", (req, res) => {
-  console.log(req.body);
+  console.log("Received POST request to /data");
+  console.log("Request body:", req.body);
   res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at port ${port}`);
+  console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
 });
